051be396d5d8902a8c350dffcab2366194a93114
LisettePihor <pihor.lisette@gmail.com>
2023-03-10 11:46:42 +0200

    tähestik ja juhised

--- src/Juhised.java
+++ src/Juhised.java
@@ -7,4 +7,10 @@ public class Juhised {
         System.out.println();
         System.out.println("Lõpetamiseks sisesta L");
     }
+    static void sisestus(){
+        System.out.println("Sidekriips tähistab pikka signaali ja tärn lühikest");
+    }
+    static void ülesanne(){
+        System.out.println("Valikuvariantide juurde tagasi saamiseks sisesta B");
+    }
 }
