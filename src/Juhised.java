b604e514c20b9ac6cabbe0cc8d067ae48f869f55
LisettePihor <pihor.lisette@gmail.com>
2023-03-10 11:08:27 +0200

    algsed juhised

---
+++ src/Juhised.java
@@ -0,0 +1,10 @@
+public class Juhised {
+    static void mainJuhised() {
+        System.out.println("Vali ülesanne:");
+        System.out.println("N - näita tähestikku");
+        System.out.println("T - tähed morse koodiks");
+        System.out.println("K - morse kood tähtedeks");
+        System.out.println();
+        System.out.println("Lõpetamiseks sisesta L");
+    }
+}
--- src/Tähed.java
+++ src/Tähed.java
@@ -1,5 +1,5 @@
 public class Tähed {
-    private final String[][] tähed = {{"A","*-"},{"B","-***"}};
+    private final String[][] tähed = {{"A","*-"},{"B","-***"},{"C"}, {"D"}, {"E"}};
     String suvaline(){
         return null;
     }
