31dc09e07f8c446cb09b59dd56d7f434f8e74d59
vanessaphtn <vanessa.apuhtin@gmail.com>
2023-03-10 11:51:26 +0200

    suvalise indeksi meetod valma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Juhised.mainJuhised();
+        Tähed tähed = new Tähed();
+        System.out.println(Arrays.toString(tähed.suvaline()));
     }
 }
--- src/Tähed.java
+++ src/Tähed.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+
+import static java.lang.Math.random;
+
 public class Tähed {
     private final String[][] tähed = {{"A", "*-"}, {"B", "-***"}, {"C", "-*-*"}, {"D", "-**"}, {"E", "*"},
             {"F", "**-*"}, {"G", "--*"}, {"H", "****"}, {"I", "**"}, {"J", "*---"}, {"K", "-*-"}, {"L", "*-**"},
@@ -5,7 +9,8 @@ public class Tähed {
             {"Z","--**"},{"T","-"},{"U","**-"},{"V","***-"},{"W","*--"},{"Ä","*-*-"},{"Ö","---*"},{"Õ","---*"},
             {"Ü","**--"},{"X","-**-"},{"Y","-*--"}};
 
-    String suvaline() {
-        return null;
+    String[] suvaline(){
+        int suvalineIndeks  = (int) (random() * tähed.length);
+        return tähed[suvalineIndeks];
     }
 }
