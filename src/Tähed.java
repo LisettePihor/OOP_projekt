d7530bc41946efaacfd62ca79c8dcbe26e20bbb8
vanessaphtn <vanessa.apuhtin@gmail.com>
2023-03-13 23:49:01 +0200

    õpitud tähti ei kuvata mitu korda

--- src/Tähed.java
+++ src/Tähed.java
@@ -11,6 +11,9 @@ public class Tähed {
             {"Z", "--**"}, {"T", "-"}, {"U", "**-"}, {"V", "***-"}, {"W", "*--"}, {"Ä", "*-*-"}, {"Ö", "---*"}, {"Õ", "---*"},
             {"Ü", "**--"}, {"X", "-**-"}, {"Y", "-*--"}};
 
+    // private String[][] tähed = {{"A", "*-"}, {"B", "-***"}, {"C", "-*-*"}, {"D", "-**"}}; katsetamiseks hea
+
+
     String[] suvaline() {
         int suvalineIndeks = (int) (random() * tähed.length);
         return tähed[suvalineIndeks];
@@ -26,9 +29,11 @@ public class Tähed {
         //vihjed ja tähtede eemaldamine
         int j = 0;
         if (i == 0) j = 1;
+        ArrayList<String> õpitudTähed = new ArrayList<>();
 
         while (true) {
-            String[] paar = suvaline();
+            String[] paar = suvaline();     //õigete vastuste korral tähed ei kordu
+            if (õpitudTähed.contains(paar[0])) continue;
             System.out.println(paar[i]);
 
             while (true) {
@@ -39,13 +44,17 @@ public class Tähed {
 
                 if (sisestus.equals(paar[j])) {
                     System.out.println("Õige vastus");
+                    õpitudTähed.add(paar[0]);
                     break;
                 } else {
                     System.out.println("Proovi uuesti");
                     System.out.println(paar[i]);
                 }
             }
-
+            if (õpitudTähed.size() == tähed.length) {     //kui kõik tähed on korra õigesti kirjutatud, siis programm lõppeb ise
+                System.out.println("Kõik on õpitud!");
+                break;
+            }
             Scanner input = new Scanner(System.in);
             System.out.println("Soovid jätkata? (jah/ei) ");
             String jätkamine = input.nextLine();
@@ -55,7 +64,6 @@ public class Tähed {
                 System.out.println("Tagasi algusesse :)");
                 break;
             }
-
         }
     }
 }
